8d14a45ee96a5d509b8ac350408992ee5399ebe0
Antoine Queru <antoine@queru.fr>
2025-09-25 06:37:30 +0200

    Fix : rebuild and manual fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex justify-center h-screen w-full overflow-auto">
       <div className="flex justify-center items-center">
-        <Image src="/png/CGT_SRX_BLK.png" className="dark:invert" alt="Logo CGT" width={200} height={200} />
+        <img src="/png/CGT_SRX_BLK.png" className="dark:invert" alt="Logo CGT" width={200} height={200} />
       </div>
     </div>
   );
